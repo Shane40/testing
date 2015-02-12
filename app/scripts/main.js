d6ebbd095b7063cadd401d845212f0a937318d65
Shane Blatt <shane.blatt@yahoo.com>
2015-02-11 20:50:06 -0500

    fumbling

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,94 +1,47 @@
-// console.log('I am ready');
+// Baby steps
+// 1. Be able to create a list.
+// 2. Be able to add to the list ... 
+// 3. Be able to remove from the list ... 
+// 4. Be able to toggle from complete to incomplete /buttons
 
-// var Animal = function (options){
-// 	var args = options || {};
-// 	this.name = args.name;
-// 	this.status = args.status || 'caged';
-// 	this.escape = function (){
-// 		this.status = "free";
-// 	};
-
-// };
-
-// var Cage = function (){
-
-
-// }; 
+var newList =[];
+var input;
 var userInput;
 
 $('#submit').on('click', function (){ //item your are targeting
   userInput = $('.text').val();// what you're doing to it
-  $('#todoList').append('<ul>' + userInput + '</ul>');
-console.log(userInput);
 
+	var newItem = new Todo ({
+    name: input,
 });
 
+$('#todoList').append('<ul>' + userInput + '</ul>');
+console.log(userInput);
+});
 
+var newList = [];
+var total = newList.push();
+console.log(total);
 
+$('#todoList').append('<ul>' + userInput + '</ul>');
 
-
-// var textInput;
-
-// $('.submit').on('click', function (){ 
-//   textInput = $('.userinput').val();
-//   $('ul').append('<li>' + textInput + '</li>');
-// });
-
-
-
-// (function (){
-//   'use strict';
-
-//   describe('About ToDo Items', function (){
-//     describe('About Creating Items', function (){
-//       it('should create an instance of ToDo', function (){
-
-//           var todo = new ToDo(options){
-//             var args = options || {};
-//             this.task = args.task;
-//             this.status = "incomplete";
-//             this.id = _.random(0, 9999);
-//             this.toggleStatus = function () {
-//                 if (this.status === "incomplete"){
-//                   this.status = "complete";
-//                 } else {
-//                   this.status = 'incomplete';
-//                 }
-//               };
-//             };
-
-
-
-
-console.log ("Let's have fun with cats");
-
-// var Cat = function (options) {
-//   options = options || {};
-//   this.color = options.color || 'brown';
-//   this.teeth = "pearly white";
-//   this.claws = "declawed";
-//   this.fat = true || options.fat;
-//   this.status = 'grumpy';
-//   this.pet = function () {
-//     this.status = 'happy';
-//   };
-// };
-
-// var samantha = new Cat ({
-//   color: "white",
-//   hungry: false,
-// });
-
-// var spencer = new Cat ({
-// 	color: "gray",
-// 	fat: true
-// });
 
 var Todo = function (options){
   options = options || {};
   this.done = false;
   this.name = options.name;
-  
+  this.input = options.input;
 
-}
+};
+
+var newItem = new Todo ({
+    name: input,
+
+});
+
+
+// I should be able to add a todo item
+// I should be able to mark the todo item complete
+// I should be able to toggle the todo item back to uncomplete
+// I should be able to delete the todo item so it's not in view any more. > Hint: Just set a property deleted or something, don't actually remove it from the Array
 
